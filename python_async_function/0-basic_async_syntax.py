d7ca41a19960d945622e24f119d525581531580e
striker06 <105947149+striker06@users.noreply.github.com>
2024-08-17 21:20:43 +0400

    fix 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,15 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random amount of seconds."""
+    """_summary_
+
+    Args:
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        float: _description_
+    """
     delay = random.random() * max_delay
     await asyncio.sleep(delay)
-    return delay
+    return float(delay)
     
