07fb3a863c41719bd37cc0750f30bcc93fcf8aea
striker06 <105947149+striker06@users.noreply.github.com>
2024-08-17 19:41:13 +0400

    betty 0, 1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string to be concatenated.
+        str2 (str): The second string to be concatenated.
+
+    Returns:
+        str: The concatenated string.
+    """
     return str1 + str2
