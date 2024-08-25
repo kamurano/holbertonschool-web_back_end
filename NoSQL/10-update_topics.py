c8e649b8ba01f39c20b92a7b89959a6fffd6d723
kamurano <kamrannn.mk@gmail.com>
2024-08-25 23:23:02 +0400

    ss

--- NoSQL/10-update_topics.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module containing functions for updating topics."""
-
-
-def update_topics(mongo_collection, name, topics):
-    """Updates topics in MongoDB."""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
--- NoSQL/8-all.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module for list_all function"""
-
-
-def list_all(mongo_collection):
-    """ List all elements in a collection """
-    return list(mongo_collection.find())
